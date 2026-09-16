490fe3e2beaf663d13d5268c889fb16735255b6d
Lucas Amaral <amarallucas@id.uff.br>
2026-09-16 15:06:58 +0000

    Add Neuropsicologia

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ with tab2:
         setor_choices = [
             'Arquivo','Departamento Médico',    'Eletro',    'Fisioterapia Dermatofuncional',    'Fisioterapia Geral',    'Fisioterapia Motora',
             'Fisioterapia NI',    'Fisioterapia Pélvica',    'Fisioterapia Vestibular',    'Fonoaudiologia',    'Fonoaudiologia NI', 'Fonoaudiologia RI',
-            'Fonoaudiologia TI',    'Massagem',    'Neuropsicopedagogia',    'Oficina Ortopédica',    'Pilates',    'Psicologia',
+            'Fonoaudiologia TI',    'Massagem', 'Neuropsicologia',    'Neuropsicopedagogia',    'Oficina Ortopédica',    'Pilates',    'Psicologia',
             'Psicologia NI', 'Psicologia RI',    'Psicologia TI',    'Psicomotricidade',    'Reintegrar',    'Respiratória',    'RPG',    'Serviço Social',
             'Terapia Ocupacional',    'Terapia Ocupacional TI'
             ]
